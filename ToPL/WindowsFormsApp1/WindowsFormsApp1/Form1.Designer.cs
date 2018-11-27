614dc3d19521b3a2173bf3a6893291d1adb65c99
Евгений Евгений <eas_24@mail.ru>
2018-11-27 18:08:31 +0700

    Added first task for ToPL

---
+++ ToPL/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 10);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(241, 22);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 38);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(241, 22);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(12, 117);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(241, 233);
+            this.textBox3.TabIndex = 2;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 356);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(274, 82);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "GO!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(292, 12);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(371, 426);
+            this.textBox4.TabIndex = 4;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(112, 66);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(42, 22);
+            this.textBox5.TabIndex = 5;
+            this.textBox5.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(259, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 17);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "NT";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(259, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(27, 17);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "NV";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 69);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(94, 17);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Range  From:";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(195, 66);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(58, 22);
+            this.textBox6.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(160, 69);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 17);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "To:";
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(15, 89);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(61, 22);
+            this.textBox7.TabIndex = 11;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(91, 92);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(114, 17);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Targeted symbol";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(679, 450);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ ToPL/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+public struct Cep
+{
+    public string A;
+    public int B;
+    public int C;
+};
+
+public struct Pravila
+{
+    public string A;
+    public List<string> B;
+    public int C;
+    public int D;
+};
+
+
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ;
+        }
+        public static string Replaceonce(string A, string B, string C)
+        {
+            return A.Substring(0, A.IndexOf(B)) + C + A.Substring(A.IndexOf(B) + B.Length);
+        }
+        public static bool Checking(string A, char[] NT, int amount)
+        {
+            for(int i = 0; i < amount; ++i)
+            {
+                if (A.IndexOf(NT[i]) > -1)
+                    return true;
+            }
+            return false;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int nA = 0, nB = 0;
+            string Target = this.textBox7.Text;
+            nA = Convert.ToInt32(this.textBox5.Text);
+            nB = Convert.ToInt32(this.textBox6.Text);
+            string VT = this.textBox1.Text;
+            string NT = this.textBox2.Text;
+            string P = this.textBox3.Text;
+            Exception E = new Exception();
+            if(NT.IndexOf(Target) == -1)
+            {
+                Target = "S";
+            }
+            string S = "S";
+            int PN = 6;
+            char[] vt, nt;
+            vt = new char[VT.Length];
+            nt = new char[NT.Length];
+            int Cnt = NT.Length;
+            for(int i = 0; i < VT.Length; i++)
+            {
+                vt[i] = VT[i];
+            }
+            for (int i = 0; i < NT.Length; i++)
+            {
+                nt[i] = NT[i];
+            }
+            int t = 0, lMax = 0;
+            List<Pravila> Rules = new List<Pravila>();
+            P = P.Replace("\n", "");
+            P = P.Replace("\r", "");
+            P = P.Replace(" ", "");
+            List<string> Answers = new List<string>();
+            while(P.Length > 0)
+            {
+                t = 0;
+                Pravila temp = new Pravila();
+                int pos = P.IndexOf('=');
+                temp.A = P.Substring(0, pos);
+                P = P.Substring(pos+1);
+                int tpos = P.IndexOf(';');
+                temp.B = new List<string>();
+                while (P[0] != ';')
+                {
+                    if (P.IndexOf('|') > tpos || P.IndexOf('|') == -1)
+                    {
+                        temp.B.Add(P.Substring(0, P.IndexOf(';')));
+                        P = P.Substring(P.IndexOf(';') + 1);
+                        break;
+                    }
+                    else
+                    {
+                        temp.B.Add(P.Substring(0, P.IndexOf('|')));
+                        t = P.IndexOf('|') + 1;
+                        P = P.Substring(P.IndexOf('|') + 1);
+                    }
+                    tpos -= t;
+                }
+                for(int i = 0; i < temp.B.Count(); ++i)
+                {
+                    temp.B[i] = temp.B[i].Replace("`", "");
+                }
+                Rules.Add(temp);
+            }
+            List<Cep> ceps = new List<Cep>();
+            
+            Cep slovo = new Cep() { A = Target, B = 0 };
+            ceps.Add(slovo);
+            int index = 0;
+            while (ceps.Count() > 0)
+            {
+                for(int i = 0; i < Rules.Count(); i++)
+                {
+                    for (int j = 0; j < Rules[i].B.Count(); j++)
+                    {
+                        if (Rules[i].B[j].Length >= lMax)
+                            lMax = Rules[i].B[j].Length;
+                        if (ceps[index].A.Contains(Rules[i].A))
+                        {
+                            string subs = ceps[index].A;
+                            subs = Replaceonce(subs, Rules[i].A, Rules[i].B[j]);
+                            Cep temp = new Cep() { A = subs, B = 0, C = 0 };
+                            if (Checking(temp.A, nt, Cnt))
+                            {
+                                if (temp.A.Length <= (PN + lMax))
+                                {
+                                    ceps.Add(temp);
+                                }
+                            }
+                            else
+                            {
+                                if (temp.A.Length <= nB && temp.A.Length >= nA)
+                                    Answers.Add(temp.A);
+                            }
+                        }
+                    }
+                }
+                ceps.Remove(ceps[0]);
+            }
+            for (int i = 0; i < Answers.Count(); ++i)
+                textBox4.Lines = Answers.ToArray();
+            
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
